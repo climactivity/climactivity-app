9abb1ace39f4a2a54cac96de78281cd6a0683fd8
vovinhd <timo.volk@gmail.com>
2020-10-14 22:59:04 +0200

    zooming

--- Camera2D.gd
+++ Camera2D.gd
@@ -2,6 +2,8 @@ extends Camera2D
 
 var pressed
 var cloud_moving 
+var zoom_step = 1.1
+
 
 func _ready():
 	if (is_instance_valid(GameManager.cloud)): 
@@ -12,8 +14,18 @@ func _cloud_moving(is_moving):
 
 func move(offset: Vector2) -> void: 
 	print("Move by ", offset)
-	self.offset += offset
+	self.offset += offset * zoom.x
 
+func zoom_at_point(zoom_change, point):
+	var c0 = global_position # camera position
+	var v0 = get_viewport().size # vieport size
+	var c1 # next camera position
+	var z0 = zoom # current zoom value
+	var z1 = z0 * zoom_change # next zoom value
+
+	c1 = c0 + (-0.5*v0 + point)*(z0 - z1)
+	zoom = z1
+	global_position = c1
 
 func _on_TileMap_move_view(offset):
 	move(offset)
@@ -21,9 +33,18 @@ func _on_TileMap_move_view(offset):
 
 func _unhandled_input(event):
 	
+	if event is InputEventMouse:
+		if event.is_pressed():
+			var mouse_position = event.position
+			if event.button_index == BUTTON_WHEEL_UP:
+				zoom_at_point(1/zoom_step,mouse_position)
+			else : if event.button_index == BUTTON_WHEEL_DOWN:
+				zoom_at_point(zoom_step,mouse_position)
+					
 	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			pressed = event.pressed
 
-		pressed = event.pressed
 		if(event.is_action_pressed("Zoom In")): 
 				print("Zoom In")
 		if(event.is_action_pressed("Zoom Out")): 
