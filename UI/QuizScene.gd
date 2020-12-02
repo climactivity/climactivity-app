5f1c199190dfe32e0ae41f6b68688d89cd75b840
vovinhd <timo.volk@gmail.com>
2020-12-02 19:18:12 +0100

    quiz data is received in app

--- UI/QuizScene.gd
+++ UI/QuizScene.gd
@@ -1,6 +1,8 @@
 extends Panel
 
 onready var req = $VBoxContainer/HTTPRequest
+onready var header = $VBoxContainer/Header
+onready var kiko_dialog = $"VBoxContainer/Content/VSplitContainer/ContentHolder/kiko_avatar - placeholder/SpeechBubbleHolder/DialogLine"
 
 var request_str = "%s://%s/infobyte/%s"
 var has_data = false
@@ -9,11 +11,11 @@ var quiz_data
 
 func receive_navigation(quiz_data):
 	#req.request(request_str % ["https", "localhost", quiz_data.quiz._id] )
+	header.set_screen_label(quiz_data.quiz.name)
 	Api.getQuizData(req, quiz_data.quiz._id)
 
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-
 	var json = JSON.parse(body.get_string_from_utf8())
 	if(json.error): 
 		Logger.error("Server error: "+ json.error, self)
@@ -21,3 +23,11 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 		has_data = true
 		quiz_data = json.result
 		Logger.print("Loaded " + quiz_data.name, self)
+		_on_quiz_data(quiz_data)
+
+func _on_quiz_data(quiz_data):
+	
+	#print(quiz_data)
+	#update header
+	header.set_screen_label(quiz_data.name)
+	kiko_dialog.text = quiz_data.frontmatter
