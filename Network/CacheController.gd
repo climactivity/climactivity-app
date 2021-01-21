adec78d3e9e2f0b6b5a3b74b7c15e41c69749ca2
vovinhd <timo.volk@gmail.com>
2021-01-21 13:51:19 +0100

    added more datatypes to cache

--- Network/CacheController.gd
+++ Network/CacheController.gd
@@ -1,21 +1,31 @@
 extends Node
 
 const aspect_resource_type = preload("res://Network/Types/RLocalizedAspectData.gd") 
+const bp_cache_manifest = preload("res://Network/Types/CacheManifest.gd")
 
+var fixed_cache_manifest = load("res://Network/Cache/manifest.res")
+var dynamic_cache_manifest = load("user://Network/Cache/manifest.res")
+var writealbe_cache_manifest
 var err = OK
-#var fs = "res" if Engine.is_editor_hint() else "user" 
-var fs = "user"
+var fs = "user" 
+var res_writable = false
 onready var req = $HTTPRequest
 
 func update():
+	res_writable = OS.is_debug_build()
+	
+	if (res_writable):
+		fs = "res"
+		writealbe_cache_manifest = fixed_cache_manifest
 	Logger.print("Rebuilding Cache...", self)
 	_make_dirs()
+	_make_manifest()
 	$HTTPRequest.connect("request_completed", self, "_on_update_aspects")
 	Api.getEndpoint("cache-aspects", req, [], true)
 
 
 func _make_dirs(): 
-	var dirs = ["Aspects", "Infobytes", "Quests"]
+	var dirs = ["Aspects", "Infobytes", "Quests", "TreeTemplates"]
 	var dir = Directory.new()
 	if fs == "user":
 		for d in dirs:
@@ -24,6 +34,20 @@ func _make_dirs():
 		if(dir.open("res://Network/Cache")):
 			for d in dirs:
 				dir.make_dir(d)
+
+func _make_manifest(): 
+	if(dynamic_cache_manifest == null): 
+		dynamic_cache_manifest = bp_cache_manifest.new()
+	writealbe_cache_manifest = dynamic_cache_manifest
+	if (res_writable):
+		if(fixed_cache_manifest == null): 
+			fixed_cache_manifest = bp_cache_manifest.new()
+		writealbe_cache_manifest = fixed_cache_manifest
+
+func _get_updated_resource_list(): 
+	pass
+func _check_manifest(): 
+	pass
 
 
 func _on_update_aspects(result, response_code, headers, body):
@@ -41,16 +65,8 @@ func _save_aspect_data(data):
 	for aspect_data in data: 
 		var path = fs + "://Network/Cache/Aspects/%s.res" % aspect_data["name"]
 		Logger.print("Saving resource for %s at %s" % [aspect_data["name"], path ], self)
-		var aspect_resource = aspect_resource_type.new(
-			aspect_data["bigpoint"] if aspect_data.has("bigpoint") else "", # p_bigpoint
-			aspect_data["name"] if aspect_data.has("name") else "", # p_name = "",
-			aspect_data["title"] if aspect_data.has("title") else "", # p_title = "",
-			aspect_data["forLanguage"] if aspect_data.has("forLanguage") else "", # p_language = "DE",
-			aspect_data["forRegion"] if aspect_data.has("forRegion") else "", # p_region = "DE",
-			aspect_data["message"] if aspect_data.has("message") else "", # p_message = "",
-			aspect_data["localizedTrackingData"] if aspect_data.has("localizedTrackingData") else {}, # p_tracking = {},
-			aspect_data["infoGraph"] if aspect_data.has("infoGraph") else {} # p_info_graph = {}
-		)
+		var aspect_resource = aspect_resource_type.new(aspect_data)
+		writealbe_cache_manifest.insert(aspect_data["_id"], "RLocalizedAspect")
 		ResourceSaver.save(path, aspect_resource, 32)
 
 func _on_update_info(result, response_code, headers, body):
@@ -62,4 +78,6 @@ func _on_update_info(result, response_code, headers, body):
 		_done()
 		
 func _done(): 
+	var path = fs + "://Network/Cache/manifest.res"
+	ResourceSaver.save(path, writealbe_cache_manifest, 32)
 	get_parent().emit_signal("finished_cache")
--- Network/Types/RLocalizedAspectData.gd
+++ Network/Types/RLocalizedAspectData.gd
@@ -25,21 +25,33 @@ export (String) var message
 export (Dictionary) var tracking
 export (Dictionary) var info_graph
 
-func _init(
-	p_bigpoint = "",
-	p_name = "",
-	p_title = "",
-	p_language = "DE",
-	p_region = "DE",
-	p_message = "",
-	p_tracking = {},
-	p_info_graph = {}
-):
-	bigpoint = p_bigpoint
-	name = p_name
-	title = p_title
-	language = p_language
-	region = p_region
-	message = p_message
-	tracking = p_tracking
-	info_graph = p_info_graph
+
+
+#func _init(
+#	p_bigpoint = "",
+#	p_name = "",
+#	p_title = "",
+#	p_language = "DE",
+#	p_region = "DE",
+#	p_message = "",
+#	p_tracking = {},
+#	p_info_graph = {}
+#):
+#	bigpoint = p_bigpoint
+#	name = p_name
+#	title = p_title
+#	language = p_language
+#	region = p_region
+#	message = p_message
+#	tracking = p_tracking
+#	info_graph = p_info_graph
+	
+func _init(dict):
+	bigpoint = dict["bigpoint"] if dict.has("bigpoint") else "" # p_bigpoint
+	name = dict["name"] if dict.has("name") else "" # p_name = "",
+	title = dict["title"] if dict.has("title") else "" # p_title = "",
+	language = dict["forLanguage"] if dict.has("forLanguage") else "" # p_language = "DE",
+	region = dict["forRegion"] if dict.has("forRegion") else "" # p_region = "DE",
+	message = dict["message"] if dict.has("message") else "" # p_message = "",
+	tracking = dict["localizedTrackingData"] if dict.has("localizedTrackingData") else {} # p_tracking = {},
+	info_graph = dict["infoGraph"] if dict.has("infoGraph") else {} # p_info_graph = {}
