cb779e64958faa105b208bffa6ad731f543a2d11
vovinhd <timo.volk@gmail.com>
2021-02-03 17:26:08 +0100

    Updates moved to own reource type

--- Network/Types/RTrackingStates.gd
+++ Network/Types/RTrackingStates.gd
@@ -1,20 +1,17 @@
 extends Resource
 
 export (Dictionary) var tracking_states = {}
+export (Array) var tracking_updates = []
+export (int) var last_update = 0
 export (Dictionary) var board_entites = {}
 export (Dictionary) var inventory = {}
 export (Dictionary) var level = {}
-##example value 
-#{
-#	"_id": 
-#		{
-#			"bigpoint": bigpoint,
-#			"current": history[0],
-#			"history": [{
-#				"set_at": time_stamp,
-#				"level?": level, # <- int falls discrete werte, float(0,1) falls contiuum
-#				"reward": reward,
-#				"value?": value
-#			}]
-#		}
-#} 
+
+
+func from_dict(dict): 
+	Logger.error("from_dict not impemented!", self)
+	pass
+
+func to_dict(dict): 
+	Logger.error("to_dict not impemented!", self)
+	pass
