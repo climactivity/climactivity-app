d5af5fde5e656d0c5d2a255624a181b93c84169b
vovinhd <timo.volk@gmail.com>
2021-04-08 16:14:12 +0200

    added repeating wrong questions

--- Services/RewardService.gd
+++ Services/RewardService.gd
@@ -1,9 +1,16 @@
 extends Node
-
+var reward = preload("res://Network/Types/RReward.gd")
 var player_state 
+
+signal reward_added(reward)
 
 func _ready(): 
 	player_state = PSS.get_player_state_ref()
 	
-func add_reward(reward):
+func add_reward(reward, show = false):
 	player_state.add_reward(reward)
+	if show: emit_signal("reward_added", reward)
+
+func DEBUG_default_reward():
+	Logger.print("Generated default reward", self)
+	return reward.new()
