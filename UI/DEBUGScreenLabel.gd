a20c3c9e59e050ab06e2a4de9428f12c9e83da55
vovinhd <timo.volk@gmail.com>
2021-04-14 11:10:03 +0200

    changed mail to link to not include '='

--- UI/DEBUGScreenLabel.gd
+++ UI/DEBUGScreenLabel.gd
@@ -5,4 +5,4 @@ func _ready():
 
 
 func _on_DEBUGScreenLabel_pressed():
-	OS.shell_open("mailto:climactivity-bugs@fire.fundersclub.com?subject=Bug%20Report&body=Version:%20"+ "v=0.1.0" +"%0AWo:%20" + get_parent().name)
+	OS.shell_open("mailto:climactivity-bugs@fire.fundersclub.com?subject=Bug%20Report&body=Version:%20"+ "v: 0.1.0" +"%0AWo:%20" + get_parent().name)
