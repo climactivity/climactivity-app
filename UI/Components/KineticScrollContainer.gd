04c248f1c7825702047116a13792875f41a37b2b
vovinhd <timo.volk@gmail.com>
2022-02-08 13:14:03 +0100

    fix scroll bugs

--- UI/Components/KineticScrollContainer.gd
+++ UI/Components/KineticScrollContainer.gd
@@ -27,7 +27,7 @@ var last_relative = Vector2.ZERO
 var swiping = false
 
 var _swipePoint = null
-var tween
+var tween: Tween
 
 #func scroll_to(node): 
 #	var target = node.rect_position.y
@@ -74,78 +74,20 @@ func _input(event):
 	if  (event is InputEventMouseButton) and !event.pressed:
 #		last_relative = _swipePoint - event.position
 		if last_relative != Vector2.ZERO:
-
 			if tween == null:
 				tween = Tween.new()
 				add_child(tween)
 			if (scrollDirection == "Vertical"):
 				var velocity = last_relative.y * last_relative.y * (-1.0 if last_relative.y < 0 else 1)
 				var time = kineticScrollTime * abs(last_relative.y)/10 
-#				print(last_relative.y , "  " , time)
 				var end_offset = self.get_v_scroll() - kineticScrollBias * velocity
+				tween.remove_all()
 				tween.interpolate_method(self, "set_v_scroll", self.get_v_scroll(), 
 					end_offset, time, 
 					Tween.TRANS_QUAD, Tween.EASE_OUT)
 				tween.start()
 				emit_signal("scrolled", end_offset)
 			last_relative = Vector2.ZERO
-#	if (not(event is InputEventMouseButton)) and (not(event is InputEventMouseMotion)):
-#		return
-#	# Check that the cursor is over the scroll area
-#	if (!self.get_global_rect().has_point(event.position)):
-#		if swiping:
-#			emit_signal("swiping", false)
-#		swiping = false
-#		_swipePoint = null
-#		return
-#	# By pressing set the necessary variables
-#	if (event is InputEventMouseButton) and (event.pressed == true): 
-#		if !swiping:
-#			emit_signal("swiping", true)
-#		swiping = true
-#		if ((event.button_index == BUTTON_LEFT) or (event.button_index == BUTTON_RIGHT)):
-#			_swipePoint = event.position
-#		if ((event.button_index == BUTTON_WHEEL_UP) or (event.button_index == BUTTON_WHEEL_DOWN)):
-#			_swipePoint = Vector2(self.get_h_scroll(), self.get_v_scroll())
-#	if (swiping) and (event is InputEventMouseButton) and (event.pressed == false):
-#		# Swipe off if the cursor position has not changed
-#		if ((_swipePoint - event.position).length() < swipeTolerance):
-#			if swiping:
-#				emit_signal("swiping", false)
-#			swiping = false
-#			_swipePoint = null
-#			return
-#		# Create a tween responsible for kinetic scrolling
-#		var tween = Tween.new()
-#		add_child(tween)
-#		if ((event.button_index == BUTTON_LEFT) or (event.button_index == BUTTON_RIGHT)):
-#			if (scrollDirection == "Horizontal"):
-#				tween.interpolate_method(self, "set_h_scroll", self.get_h_scroll(), 
-#					self.get_h_scroll() - kineticScrollBias*(event.position.x - _swipePoint.x), kineticScrollTime, 
-#					Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-#			if (scrollDirection == "Vertical"):
-#				tween.interpolate_method(self, "set_v_scroll", self.get_v_scroll(), 
-#					self.get_v_scroll() - kineticScrollBias*(event.position.y - _swipePoint.y), kineticScrollTime, 
-#					Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-#		if ((event.button_index == BUTTON_WHEEL_UP) or (event.button_index == BUTTON_WHEEL_DOWN)):
-#			if (scrollDirection == "Horizontal"):
-#				tween.interpolate_method(self, "set_h_scroll", self.get_h_scroll(), 
-#					self.get_h_scroll() + kineticScrollBias*(self.get_h_scroll() - _swipePoint.x), kineticScrollTime, 
-#					Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-#			if (scrollDirection == "Vertical"):
-#				tween.interpolate_method(self, "set_v_scroll", self.get_v_scroll(), 
-#					self.get_v_scroll() + kineticScrollBias*(self.get_v_scroll() - _swipePoint.y), kineticScrollTime, 
-#					Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-#		tween.interpolate_callback(self, kineticScrollTime, "set", "swiping", false)
-#		tween.interpolate_callback(tween, kineticScrollTime, "queue_free")
-#		tween.start()
-#		_swipePoint = null
-#	# Scrolling while moving the cursor
-#	if (_swipePoint != null) and (event is InputEventMouseMotion):
-#		if (scrollDirection == "Horizontal"):
-#			self.set_h_scroll(self.get_h_scroll() + linearScrollBias * (-event.position.x + _swipePoint.x))
-#		if (scrollDirection == "Vertical"):
-#			self.set_v_scroll(self.get_v_scroll() + linearScrollBias * (-event.position.y + _swipePoint.y))
 
 
 # The function provides disable/enable kinetic scrolling at runtime
