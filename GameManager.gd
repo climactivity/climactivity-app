b2d47b19b8d713eb89df3a190f21e963e1538043
vovinhd <timo.volk@gmail.com>
2022-04-14 11:31:19 +0200

    set version number

--- GameManager.gd
+++ GameManager.gd
@@ -18,7 +18,6 @@ func _init():
 func save_game(): 
 	var save_data = {
 		"install_id": OS.get_unique_id(),
-		"player_data": PlayerController.save(),
 		"forest_state": forest.save_forest_state(),
 	}
 	if (OS.is_debug_build()):
